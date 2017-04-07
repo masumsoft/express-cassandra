a9f94b3bb67b1b137a055cf3e6e84b2b17ce36e8
Pierre <pierre@matri.me>
2017-04-07 15:00:44 +0200

    Add isModified model method

--- src/orm/base_model.js
+++ src/orm/base_model.js
@@ -18,8 +18,12 @@ const BaseModel = function f(instanceValues) {
   instanceValues = instanceValues || {};
   const fieldValues = {};
   const fields = this.constructor._properties.schema.fields;
+  const model = this;
 
   const defaultSetter = function f1(propName, newValue) {
+    if (this[propName] !== newValue) {
+      model._modified[propName] = true;
+    }
     this[propName] = newValue;
   };
 
@@ -27,6 +31,7 @@ const BaseModel = function f(instanceValues) {
     return this[propName];
   };
 
+  this._modified = {};
   this._validators = {};
 
   for (let fieldsKeys = Object.keys(fields), i = 0, len = fieldsKeys.length; i < len; i++) {
@@ -1492,6 +1497,7 @@ BaseModel.eachRow = function f(queryObject, options, onReadable, callback) {
     if (!options.raw) {
       const ModelConstructor = this._properties.get_constructor();
       row = new ModelConstructor(row);
+      row._modified = {};
     }
     onReadable(n, row);
   }, (err, result) => {
@@ -1571,7 +1577,9 @@ BaseModel.stream = function f(queryObject, options, onReadable, callback) {
       if (!row) return row;
       if (!options.raw) {
         const ModelConstructor = self._properties.get_constructor();
-        return new ModelConstructor(row);
+        const o = new ModelConstructor(row);
+        o._modified = {};
+        return o;
       }
       return row;
     };
@@ -1642,7 +1650,9 @@ BaseModel.find = function f(queryObject, options, callback) {
       const ModelConstructor = this._properties.get_constructor();
       results = results.rows.map((res) => {
         delete (res.columns);
-        return new ModelConstructor(res);
+        const o = new ModelConstructor(res);
+        o._modified = {};
+        return o;
       });
       callback(null, results);
     } else {
@@ -1920,6 +1930,7 @@ BaseModel.update = function f(queryObject, updateValues, options, callback) {
         });
       },
       after_hook: (hookCallback) => {
+        queryObject._modified = {};
         schema.after_update(queryObject, updateValues, options, (error) => {
           if (error) {
             hookCallback(buildError('model.update.after.error', error));
@@ -2329,6 +2340,7 @@ BaseModel.prototype.save = function fn(options, callback) {
           callback(buildError('model.save.dberror', err));
           return;
         }
+        this._modified = {};
         schema.after_save(this, options, (error1) => {
           if (error1) {
             callback(buildError('model.save.after.error', error1));
@@ -2383,6 +2395,13 @@ BaseModel.prototype.toJSON = function toJSON() {
   });
 
   return object;
+};
+
+BaseModel.prototype.isModified = function isModified(propName) {
+  if (propName) {
+    return Object.prototype.hasOwnProperty.call(this._modified, propName);
+  }
+  return Object.keys(this._modified).length !== 0;
 };
 
 module.exports = BaseModel;
