dc9953d5e960b164b6d15d5846e17358f35aa8eb
Marek <marek_karczewski@yahoo.com.au>
2018-01-21 20:05:19 +0100

    feature: ES index per model

--- src/builders/elassandra.js
+++ src/builders/elassandra.js
@@ -1,86 +1,86 @@
 const debug = require('debug')('express-cassandra');
 
 const ElassandraBuilder = function f(client) {
-  this._client = client;
+	this._client = client;
 };
 
 ElassandraBuilder.prototype = {
-  create_index(indexName, callback) {
-    debug('creating elassandra index: %s', indexName);
-    this._client.indices.create({
-      index: indexName,
-      body: {
-        settings: {
-          keyspace: indexName,
-        },
-      },
-    }, (err) => {
-      if (err) {
-        callback(err);
-        return;
-      }
+	create_index(keyspaceName, indexName, callback) {
+		debug('creating elassandra index: %s', indexName);
+		this._client.indices.create({
+			index: indexName,
+			body: {
+				settings: {
+					keyspace: keyspaceName
+				},
+			},
+		}, (err) => {
+			if (err) {
+				callback(err);
+				return;
+			}
 
-      callback();
-    });
-  },
+			callback();
+		});
+	},
 
-  check_index_exist(indexName, callback) {
-    debug('check for elassandra index: %s', indexName);
+	check_index_exist(indexName, callback) {
+		debug('check for elassandra index: %s', indexName);
     this._client.indices.exists({ index: indexName }, (err, res) => {
-      if (err) {
-        callback(err);
-        return;
-      }
+			if (err) {
+				callback(err);
+				return;
+			}
 
-      callback(null, res);
-    });
-  },
+			callback(null, res);
+		});
+	},
 
-  assert_index(indexName, callback) {
-    this.check_index_exist(indexName, (err, exist) => {
-      if (err) {
-        callback(err);
-        return;
-      }
+	assert_index(keyspaceName, indexName, callback) {
+		this.check_index_exist(indexName, (err, exist) => {
+			if (err) {
+				callback(err);
+				return;
+			}
 
-      if (!exist) {
-        this.create_index(indexName, callback);
-        return;
-      }
+			if (!exist) {
+				this.create_index(keyspaceName, indexName, callback);
+				return;
+			}
 
-      callback();
-    });
-  },
+			callback();
+		});
+	},
 
-  delete_index(indexName, callback) {
-    debug('removing elassandra index: %s', indexName);
-    this._client.indices.delete({
-      index: indexName,
-    }, (err) => {
-      if (err) {
-        callback(err);
-        return;
-      }
+	delete_index(indexName, callback) {
+		debug('removing elassandra index: %s', indexName);
+		this._client.indices.delete({
+			index: indexName,
+		}, (err) => {
+			if (err) {
+				callback(err);
+				return;
+			}
 
-      callback();
-    });
-  },
+			callback();
+		});
+	},
 
-  put_mapping(indexName, mappingName, mappingBody, callback) {
-    debug('syncing elassandra mapping: %s', mappingName);
-    this._client.indices.putMapping({
-      index: indexName,
-      type: mappingName,
-      body: mappingBody,
-    }, (err) => {
-      if (err) {
-        callback(err);
-        return;
-      }
+	put_mapping(indexName, mappingName, mappingBody, callback) {
+		debug('syncing elassandra mapping: %s', mappingName);
+		this._client.indices.putMapping({
+			index: indexName,
+			type: mappingName,
+			body: mappingBody,
+		}, (err) => {
+			if (err) {
+				callback(err);
+				return;
+			}
 
-      callback();
-    });
-  },
+			callback();
+		});
+	},
 };
 
 module.exports = ElassandraBuilder;
